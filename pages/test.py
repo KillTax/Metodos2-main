5e8f8805a6d05230c1f08708210dc327e3a42866
Max Vega <maxvega091@gmail.com>
2023-05-30 20:26:08 -0600

    intento de grafica en test

--- pages/test.py
+++ pages/test.py
@@ -1,3 +1,18 @@
+## Gráfica de ejemplo
 import streamlit as st
-import sympy as sy
-st.image("Imagen.jpg")
+import matplotlib.pyplot as plt
+
+# Datos de muestra
+x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+y = [1.3, 3.5, 4.2, 5.0, 7.0, 8.8, 10.1, 12.5, 13.0, 15.6]
+
+# Crear la gráfica
+plt.plot(x, y, 'b.-')
+
+# Personalizar la gráfica
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Gráfica de ejemplo')
+
+# Mostrar la gráfica en Streamlit
+st.pyplot(plt)
