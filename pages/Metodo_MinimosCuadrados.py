ca4006353efa50316bf9c3eb46f72550377bd4fc
Max Vega <maxvega091@gmail.com>
2023-05-30 20:46:18 -0600

    test

--- pages/Metodo_MinimosCuadrados.py
+++ pages/Metodo_MinimosCuadrados.py
@@ -51,16 +51,3 @@ with tab1:
     st.title(":blue[Metodo de Minimos Cuadrados]")
     st.header("Ejemplo")
     st.markdown("")
-    # Datos de muestra
-    x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    y = [1.3, 3.5, 4.2, 5.0, 7.0, 8.8, 10.1, 12.5, 13.0, 15.6]
-
-    # Crear la gráfica
-    plt.plot(x, y, 'b.-')
-
-    # Personalizar la gráfica
-    plt.xlabel('x')
-    plt.ylabel('y')
-    plt.title('Gráfica de ejemplo')
-    # Mostrar la gráfica en Streamlit
-    st.pyplot(plt)
--- pages/test.py
+++ pages/test.py
@@ -2,8 +2,16 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import numpy as np
 
-arr = np.random.normal(1, 1, size=100)
-fig, ax = plt.subplots()
-ax.hist(arr, bins=20)
+# Datos de muestra
+x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+y = [1.3, 3.5, 4.2, 5.0, 7.0, 8.8, 10.1, 12.5, 13.0, 15.6]
 
-st.pyplot(fig)
+# Crear la gráfica
+plt.plot(x, y, 'b.-')
+
+# Personalizar la gráfica
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Gráfica de ejemplo')
+# Mostrar la gráfica en Streamlit
+st.pyplot(plt)
