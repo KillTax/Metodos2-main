a16f2feae9dcb6f551c893c93461712b49921f9f
Max Vega <maxvega091@gmail.com>
2023-05-30 20:16:13 -0600

    intento 2

--- pages/Metodo_MinimosCuadrados.py
+++ pages/Metodo_MinimosCuadrados.py
@@ -49,7 +49,7 @@ with tab1:
 with tab1:
     st.title(":blue[Metodo de Minimos Cuadrados]")
     st.header("Ejemplo")
-    st.markdown()
+    st.markdown("")
     # Datos de muestra
     x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     y = [1.3, 3.5, 4.2, 5.0, 7.0, 8.8, 10.1, 12.5, 13.0, 15.6]
