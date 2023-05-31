8012b35bd5ae4627c95b8350d0b0d5dcaffce1c6
Max Vega <maxvega091@gmail.com>
2023-05-30 20:26:58 -0600

    1

--- pages/test.py
+++ pages/test.py
@@ -1,18 +1,9 @@
-## Gráfica de ejemplo
 import streamlit as st
 import matplotlib.pyplot as plt
+import numpy as np
 
-# Datos de muestra
-x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-y = [1.3, 3.5, 4.2, 5.0, 7.0, 8.8, 10.1, 12.5, 13.0, 15.6]
+arr = np.random.normal(1, 1, size=100)
+fig, ax = plt.subplots()
+ax.hist(arr, bins=20)
 
-# Crear la gráfica
-plt.plot(x, y, 'b.-')
-
-# Personalizar la gráfica
-plt.xlabel('x')
-plt.ylabel('y')
-plt.title('Gráfica de ejemplo')
-
-# Mostrar la gráfica en Streamlit
-st.pyplot(plt)
+st.pyplot(fig)
